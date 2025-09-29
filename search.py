36ee971b4e7ead3fb292be44fba3131ebe84ae3c
Varnern23 <159178249+Varnern23@users.noreply.github.com>
2025-09-29 19:01:39 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,34 +1,48 @@
 import numpy as np
 import json
 from ollama import embed
+import csv
 
-with open("index/metadata.json", "r") as f:
+
+with open("index/metadata2.json", "r") as f:
     metadata = json.load(f)
 
-with open("index/chunks.json", "r", encoding="utf-8") as f:
+with open("index/chunks2.json", "r", encoding="utf-8") as f:
     chunks = [json.loads(line) for line in f]
-embeddings = np.load("index/embeddings.npy")
+embeddings = np.load("index/embeddings2.npy")
 k = 5
 print("Type your query or type exit to quit:\n")
-while True:
-    query = input("Enter query: ")
-    if query.lower() == "exit":
-        break
-    resp = embed(model = metadata["model"], input = query)
-    qVector = np.array(resp["embeddings"][0], dtype = np.float32)
-    qVector /= np.linalg.norm(qVector)
-    scores = np.dot(embeddings, qVector)
-    topK = np.argsort(scores)[-k:][::-1]
-    print("\n Top Chunks:\n")
-    track = set()
-    for rank, i in enumerate(topK, start = 1):
-        i = int(i)
-        chunk = chunks[i]
-        chunkID = chunk['ID']
-        if chunkID in track:
-            continue
-        track.add(chunkID)
-        print(f"Rank: {rank}, Score: {scores[i]:.4f}")
-        print(f"Chunk ID: {chunk['ID']}")
-        print(f"Text: {chunk['text']}\n")
-    
+labels = ["prompt", "expected", "hit 1", "hit 2", "hit 3", "hit 4", "hit 5"]
+with open("outputs/baseTest.csv", "w", newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(labels)
+    while True:
+        query = input("Enter query: ")
+        if query.lower() == "exit":
+            break
+        resp = embed(model=metadata["model"], input=query)
+        qVector = np.array(resp["embeddings"][0], dtype=np.float32)
+        qVector /= np.linalg.norm(qVector)
+        scores = np.dot(embeddings, qVector)
+        topK = np.argsort(scores)[-k:][::-1]
+        print("\n Top Chunks:\n")
+        track = set()
+        hits = []
+        for rank, i in enumerate(topK, start=1):
+            i = int(i)
+            chunk = chunks[i]
+            chunkID = chunk['ID']
+            if chunkID in track:
+                continue
+            track.add(chunkID)
+            hits.append(chunk)
+            print(f"Rank: {rank}, Score: {scores[i]:.4f}")
+            print(f"Chunk ID: {chunk["ID"]}\n")
+            print(f"Text: {chunk["text"]}")
+        print("\n")
+        writer.writerow( [query, "", 
+                         hits[0]['text'],
+                         hits[1]['text'],
+                         hits[2]['text'],
+                         hits[3]['text'],
+                         hits[4]['text']])
